c90022e01b17208a3e7b7c6e8e173243ad68af75
Loan Lassalle <18124884+lassalleloan@users.noreply.github.com>
2017-09-18 15:00:10 +0200

    A Beginning

---
+++ src/main/java/com/loanlassalle/smartcity/SmartCity.java
@@ -0,0 +1,17 @@
+package com.loanlassalle.smartcity;
+
+import com.loanlassalle.smartcity.view.FenetrePrincipale;
+import com.loanlassalle.smartcity.view.controllers.ConfigurationManager;
+
+/**
+ * Lance l'application et ouvre la fenêtre principale
+ */
+public class SmartCity {
+
+    public static void main(String[] args) throws Exception {
+
+        // Supprime la journalisation de la librairie C3P0 dans la console
+        ConfigurationManager.getInstance().c3p0Logging();
+        new FenetrePrincipale().fenetre.setVisible(true);
+    }
+}
